95f34af623f737230c93267219e1d044979d265d
Rashini0926 <it23649644@my.sliit.lk>
2026-07-15 10:58:20 +0530

    feat: add homepage category section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/home/Hero";
+import Categories from "../components/home/Categories";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Categories />
     </>
   );
 }
