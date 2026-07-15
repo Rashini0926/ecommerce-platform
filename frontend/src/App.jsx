59ace85cf4ed3bac8a70995f7ae8b406b669a37a
Rashini0926 <it23649644@my.sliit.lk>
2026-07-15 11:24:12 +0530

    feat: configure React application routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
 
 
 function App() {
@@ -16,8 +19,15 @@ function App() {
 
       <Route path="/register" element={<Register />} />
 
+      <Route path="/products" element={<Products />} />
+
+      <Route path="/cart" element={<Cart />} />
+
+      <Route path="/wishlist" element={<Wishlist />} />
+
     </Routes>
   );
 }
+
 
 export default App;
