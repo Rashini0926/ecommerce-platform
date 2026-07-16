f1c6e2d0dfa459679c394cb05517bda06b22dbb0
Rashini0926 <it23649644@my.sliit.lk>
2026-07-17 00:22:06 +0530

    feat: configure authentication routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Home Route
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "E-commerce API is running..."
+    message: "E-commerce API is running...",
   });
 });
 
-// Database Test Route
-app.get("/api/test-db", async (req, res) => {
-  try {
-    const connection = await db.getConnection();
-    connection.release();
-
-    res.json({
-      success: true,
-      message: "Database connected successfully"
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
-});
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
