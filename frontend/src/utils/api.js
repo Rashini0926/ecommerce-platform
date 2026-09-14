5d61ce6c709fe6e3f78552caeaef8888bdafef5e
Kodni Diyana <kodnidiyanadesilva123@gmail.com>
2026-09-14 13:28:59 +0530

    fix: export API base URL for product page

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,28 @@
 import axios from "axios";
 
+export const API_BASE_URL = "http://127.0.0.1:8000/api";
+
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-    },
+  baseURL: API_BASE_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
